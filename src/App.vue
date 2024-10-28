e76f5e4d91dfbde133093eb051083c627a8b181c
Moonmangit <81956440+moonmangit@users.noreply.github.com>
2024-10-28 09:43:40 +0700

    add : vue - router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import './style.css';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+// router
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('./pages/home/HomePage.vue'),
+    },
+    {
+      path: '/about',
+      component: () => import('./pages/about/AboutPage.vue'),
+    },
+  ],
+});
+app.use(router);
+
+app.mount('#app');
